03f7b3ad65614ac8e58418aa8d8ae3e9d71b6c59
Anshad r <anshadr2@gmail.com>
2018-05-02 23:04:46 +0530

    Initial commit

---
+++ WaaZi TV/WaaZi TV/Controllers/BaseVC/BaseVC.swift
@@ -0,0 +1,22 @@
+//
+//  BaseVC.swift
+//  WaaZi TV
+//
+//  Created by Akhil P.K on 02/05/18.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
---
+++ WaaZi TV/WaaZi TV/Controllers/Login/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  WaaZi TV
+//
+//  Created by Akhil P.K on 02/05/18.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
