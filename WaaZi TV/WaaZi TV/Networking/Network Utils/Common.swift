871b231249d374d3be99f856657609021f31a1ca
Anshad r <anshadr2@gmail.com>
2018-05-03 00:11:05 +0530

    Network Layer added

---
+++ WaaZi TV/WaaZi TV/Networking/Network Utils/Common.swift
@@ -0,0 +1,191 @@
+//
+//  Common.swift
+//  WaaZi TV
+//
+//  Created by Akhil P.K on 02/05/18.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+/// Constants define
+public struct Constants {
+    
+    /// This represent keys used into the Info.plist file of your app.
+    /// The root node is `endpoint` with `base`, `pathAPI` and `name` inside which contains
+    /// your server configuration.
+    ///
+    /// - endpoint: endpoint
+    /// - base: base
+    /// - pathAPI: api service path
+    /// - name: name of the configuration
+    /// - headers: global headers to include in a service base configuration
+    public enum ServerConfig: String {
+        case endpoint    =    "endpoint"
+        case base        =    "base"
+        case pathAPI    =    "path"
+        case name        =    "name"
+        case headers    =    "headers"
+    }
+    
+    /// Avoid initialization of this (it's just for namingspace purposes)
+    private init() {}
+    
+}
+/// Possible networking error
+///
+/// - dataIsNotEncodable: data cannot be encoded in format you have specified
+/// - stringFailedToDecode: failed to decode data with given encoding
+public enum NetworkError: Error {
+    case dataIsNotEncodable(_: Any)
+    case stringFailedToDecode(_: Data, encoding: String.Encoding)
+    case invalidURL(_: String)
+    case error(_: ResponseProtocol)
+    case noResponse(_: ResponseProtocol)
+    case missingEndpoint
+    case failedToParseJSON(_: JSON, _: ResponseProtocol)
+}
+
+/// Define the parameter's dictionary
+public typealias ParametersDict = [String : Any?]
+
+/// Define the header's dictionary
+public typealias HeadersDict = [String: String]
+
+/// Define what kind of HTTP method must be used to carry out the `Request`
+///
+/// - get: get (no body is allowed inside)
+/// - post: post
+/// - put: put
+/// - delete: delete
+/// - patch: patch
+public enum RequestMethod: String {
+    case get    = "GET"
+    case post    = "POST"
+    case put    = "PUT"
+    case delete    = "DELETE"
+    case patch    = "PATCH"
+}
+
+/// This define how the body should be encoded
+///
+/// - none: no transformation is applied, data is sent raw as received in `body` param of the request.
+/// - json: attempt to serialize a `Dictionary` or a an `Array` as `json`. Other types are not supported and throw an exception.
+/// - urlEncoded: it expect a `Dictionary` as input and encode it as url encoded string into the body.
+/// - custom->: custom serializer. `Any` is accepted, `Data` is expected as output.
+public struct RequestBody {
+    
+    /// Data to carry out into the body of the request
+    let data: Any
+    
+    /// Type of encoding to use
+    let encoding: Encoding
+    
+    /// Encoding type
+    ///
+    /// - raw: no encoding, data is sent as received
+    /// - json: json encoding
+    /// - urlEncoded: url encoded string
+    /// - custom: custom serialized data
+    public enum Encoding {
+        case rawData
+        case rawString(_: String.Encoding?)
+        case json
+        case urlEncoded(_: String.Encoding?)
+        case custom(_: CustomEncoder)
+        
+        /// Encoder function typealias
+        public typealias CustomEncoder = ((Any) -> (Data))
+    }
+    
+    /// Private initializa a new body
+    ///
+    /// - Parameters:
+    ///   - data: data
+    ///   - encoding: encoding type
+    private init(_ data: Any, as encoding: Encoding = .json) {
+        self.data = data
+        self.encoding = encoding
+    }
+    
+    /// Create a new body which will be encoded as JSON
+    ///
+    /// - Parameter data: any serializable to JSON object
+    /// - Returns: RequestBody
+    public static func json(_ data: Any) -> RequestBody {
+        return RequestBody(data, as: .json)
+    }
+    
+    /// Create a new body which will be encoded as url encoded string
+    ///
+    /// - Parameters:
+    ///   - data: a string of encodable data as url
+    ///   - encoding: encoding type to transform the string into data
+    /// - Returns: RequestBody
+    public static func urlEncoded(_ data: ParametersDict, encoding: String.Encoding? = .utf8) -> RequestBody {
+        return RequestBody(data, as: .urlEncoded(encoding))
+    }
+    
+    /// Create a new body which will be sent in raw form
+    ///
+    /// - Parameter data: data to send
+    /// - Returns: RequestBody
+    public static func raw(data: Data) -> RequestBody {
+        return RequestBody(data, as: .rawData)
+    }
+    
+    /// Create a new body which will be sent as plain string encoded as you set
+    ///
+    /// - Parameter data: data to send
+    /// - Returns: RequestBody
+    public static func raw(string: String, encoding: String.Encoding? = .utf8) -> RequestBody {
+        return RequestBody(string, as: .rawString(encoding))
+    }
+    
+    /// Create a new body which will be encoded with a custom function.
+    ///
+    /// - Parameters:
+    ///   - data: data to encode
+    ///   - encoder: encoder function
+    /// - Returns: RequestBody
+    public static func custom(_ data: Data, encoder: @escaping Encoding.CustomEncoder) -> RequestBody {
+        return RequestBody(data, as: .custom(encoder))
+    }
+    
+    /// Encoded data to carry out with the request
+    ///
+    /// - Returns: Data
+    public func encodedData() throws -> Data {
+        switch self.encoding {
+        case .rawData:
+            return self.data as! Data
+        case .rawString(let encoding):
+            guard let string = (self.data as! String).data(using: encoding ?? .utf8) else {
+                throw NetworkError.dataIsNotEncodable(self.data)
+            }
+            return string
+        case .json:
+            return try JSONSerialization.data(withJSONObject: self.data, options: [])
+        case .urlEncoded(let encoding):
+            let encodedString = try (self.data as! ParametersDict).urlEncodedString()
+            guard let data = encodedString.data(using: encoding ?? .utf8) else {
+                throw NetworkError.dataIsNotEncodable(encodedString)
+            }
+            return data
+        case .custom(let encodingFunc):    return encodingFunc(self.data)
+        }
+    }
+    
+    /// Return the representation of the body as `String`
+    ///
+    /// - Parameter encoding: encoding use to read body's data. If not specified `utf8` is used.
+    /// - Returns: String
+    /// - Throws: throw an exception if string cannot be decoded as string
+    public func encodedString(_ encoding: String.Encoding = .utf8) throws -> String {
+        let encodedData = try self.encodedData()
+        guard let stringRepresentation = String(data: encodedData, encoding: encoding) else {
+            throw NetworkError.stringFailedToDecode(encodedData, encoding: encoding)
+        }
+        return stringRepresentation
+    }
+}
---
+++ WaaZi TV/WaaZi TV/Networking/Service/Service.swift
@@ -0,0 +1,59 @@
+//  WaaZi TV
+//
+//  Created by Akhil P.K on 02/05/18.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+import Hydra
+
+/// Service is a concrete implementation of the ServiceProtocol
+public class Service: ServiceProtocol {
+
+	/// Configuration
+	public var configuration: ServiceConfig
+	
+	/// Session headers
+	public var headers: HeadersDict
+	
+	/// Initialize a new service with given configuration
+	///
+	/// - Parameter configuration: configuration. If `nil` is passed attempt to load configuration from your app's Info.plist
+	public required init(_ configuration: ServiceConfig) {
+		self.configuration = configuration
+		self.headers = self.configuration.headers // fillup with initial headers
+	}
+	
+	/// Execute a request and return a promise with the response
+	///
+	/// - Parameters:
+	///   - request: request to execute
+	///   - retry: retry attempts. If `nil` only one attempt is made. Default value is `nil`.
+	/// - Returns: Promise
+	/// - Throws: throw an exception if operation cannot be executed
+	public func execute(_ request: RequestProtocol, retry: Int?) -> Promise<ResponseProtocol> {
+		// Wrap in a promise the request itself
+		let op = Promise<ResponseProtocol>(in: request.context ?? .background, token: request.invalidationToken, { (r, rj, s) in
+			// Attempt to create the object to perform request
+			let dataOperation: DataRequest = try Alamofire.request(request.urlRequest(in: self))
+			// Execute operation in Alamofire
+			dataOperation.response(completionHandler: { rData in
+				// Parse response
+				let parsedResponse = Response(afResponse: rData, request: request)
+				switch parsedResponse.type {
+				case .success: // success
+					r(parsedResponse)
+				case .error: // failure
+					rj(NetworkError.error(parsedResponse))
+				case .noResponse:  // no response
+					rj(NetworkError.noResponse(parsedResponse))
+				}
+			})
+		})
+		guard let retryAttempts = retry else { return op } // single shot
+		return op.retry(retryAttempts) // retry n times
+	}
+	
+}
